1d6ec5b19505baf2dc4aaa0ef915b2f021d5dc9c
boojoom <aisu.yamiyo@gmail.com>
2025-05-13 00:24:24 +0200

    Hot fixy i wizualne przeróbki

--- lang/en.php
+++ lang/en.php
@@ -13,14 +13,20 @@ $lang = array(
     "contact" => "Contact",
 
     "welcome_heading" => "CYBER PhD Conference — Conference for Young Biomedical Engineering Researchers",
-    "welcome_text_1" => "We invite you to the <strong>CYBER</strong> PhD Conference — a unique event bringing together young researchers, PhD students, undergraduate students, and specialists in the field of biomedical engineering. Our interests span neuroscience, optics, medical imaging, bioinformatics, drug design, biomaterials, and antimicrobial technologies. CYBER is a platform for sharing knowledge and experience gained through interdisciplinary scientific work. We aim to showcase the latest research achievements from our Department and inspire young scientists to embark on their own PhD journey.",
-    "welcome_text_2" => "By attending lectures and discussions, you will broaden your knowledge, stay up-to-date with the latest developments in biomedical engineering, and discover exciting directions for your future scientific career. We hope to spark your passion for research and innovation! Join us by filling out the registration form. See you on <strong>June 23 2025, in building D20 at Zygmunta Janiszewskiego 8 Street!</strong>",
+    "welcome_text_1" => "We invite you to the <strong style='color: #1FA53E;'>CYBER</strong> PhD Conference — a unique event bringing together young researchers, PhD students, undergraduate students, and specialists in the field of biomedical engineering. Our interests span neuroscience, optics, medical imaging, bioinformatics, drug design, biomaterials, and antimicrobial technologies. CYBER is a platform for sharing knowledge and experience gained through interdisciplinary scientific work. We aim to showcase the latest research achievements from our Department and inspire young scientists to embark on their own PhD journey.",
+    "welcome_text_2" => "By attending lectures and discussions, you will broaden your knowledge, stay up-to-date with the latest developments in biomedical engineering, and discover exciting directions for your future scientific career. We hope to spark your passion for research and innovation! Join us by filling out the registration form. See you on <strong style='color: #1FA53E;'>June 23 2025, in building D20 at Zygmunta Janiszewskiego 8 Street!</strong>",
+    "welcome_important_title" => "<strong style='color: #1FA53E;'>IMPORTANT</strong>",
+    "welcome_important_text1" => "Registration for active participants ends on <strong style='color: #1FA53E;'>31 May</strong> and passive participants on <strong style='color: #1FA53E;'>18 June</strong>.",
+    "welcome_important_text2" => "Information regarding the acceptance of submissions will be provided by <strong style='color: #1FA53E;'>June 10</strong>.",
+    "welcome_datePlace_title" => "When and where?",
+    "welcome_datePlace_text" => "The conference will take place on June 23, 2025, in building D20 at 8 Zygmunt Janiszewski Street!",
+
 
     "announcements" => "Announcements",
-    "announcement_1_title" => "📅 Registration Opening",
-    "announcement_1_text" => "Registration for CYBER conference will begin on <strong>May 15</strong>. We encourage you to register early! Information regarding the acceptance of submissions will be provided by <strong>June 10</strong>. Bonus - the conference is <strong>free of charge</strong>!",
+    "announcement_1_title" => "📅 Registration Information",
+    "announcement_1_text" => "Registration for CYBER conference will begin on <strong style='color: #1FA53E;'>May 15</strong>. We encourage you to register early - we’re looking forward to receiving your forms by <strong style='color: #1FA53E;'>May 31</strong>!! Information regarding the acceptance of submissions will be provided by <strong style='color: #1FA53E;'>June 10</strong>. Bonus - the conference is <strong style='color: #1FA53E;'>free of charge</strong>!",
     "announcement_2_title" => "🎤 Keynote lecture",
-    "announcement_2_text" => 'We are happy to announce that the inaugural lecture, titled <i>"The positive face of protein and peptide aggregation: foldameric nanostructures and functional amyloids"</i>, will be delivered by <strong>Dr. Monika Szefczyk</strong> from the Faculty of Chemistry at Wrocław University of Science and Technology',
+    "announcement_2_text" => 'We are happy to announce that the inaugural lecture, titled <i style="color: #1FA53E;">"The positive face of protein and peptide aggregation: foldameric nanostructures and functional amyloids"</i>, will be delivered by <strong style="color: #1FA53E;">Dr. Monika Szefczyk</strong> from the Faculty of Chemistry at Wrocław University of Science and Technology',
     "announcement_2_link" => 'We invite you to learn more about our speaker’s work',
     "announcement_3_title" => "🧪 Find us in other places too!",
     "announcement_3_text" => "Follow our event on Facebook – stay up to date!",
@@ -28,23 +34,18 @@ $lang = array(
     "program_of_conf" => "Conference Program",
     "time" => "Time",
     "event" => "Event",
-    "opening" => "Conference Opening (5–10 min); possibly guest lecture (20 min)",
-    "session1" => "First Presentation Session (1h 30 min; 7/8 presentations)",
+    "opening" => "Conference Opening and guest lecture",
+    "session1" => "Vision Science & Optometry",
     "coffee" => "<strong>Coffee Break</strong>",
-    "session2" => "Second Presentation Session (1h 30 min; 7/8 presentations)",
+    "session2" => "Neuroscience",
     "lunch" => "<strong>Lunch</strong> <br><span class='text-muted'>(for speakers and professors only)</span>",
-    "session3" => "Third Presentation Session (1h 30 min; 7/8 presentations)",
-    "session4" => "Fourth Presentation Session <br><span class='text-muted'>(7/8 presentations)</span>",
+    "session3" => "Biomaterials, Biomedical Imaging & Diagnostic Technologies",
+    "session4" => "Bioinformatics & Molecular Medicine",
     "closing" => "Awards, Summary, Conference Closing",
 
     "registration_on_conf" => "Conference Registration",
-    "registration_intro" => "Register to <strong>CYBER</strong> now! Click the button below to access the registration form, where you’ll find all the details about participation.",
+    "registration_intro" => "Register to <strong style='color: #1FA53E;'>CYBER</strong> now! Click the button below to access the registration form, where you’ll find all the details about participation.",
     "registration_button" => "Register Here",
-
-    "organizing_committee" => "Organizing Committee",
-    "coordinator" => "Main Coordinator",
-    "logistics" => "Logistics & Guest Relations",
-    "media" => "Media & Promotion",
 
     "contact_intro" => "Have questions about the conference? Contact us using the form below or write to us via email.",
     "contact_name" => "Full Name",
@@ -53,10 +54,12 @@ $lang = array(
     "contact_send" => "Send Message",
     "contact_alt" => "Contact illustration",
 
+    "organizing_committee" => "Organizing Committee",
+
     "committee_members" => [
-        ["name" => "Zofia Dobrowolska", "role" => "Main coordinator"],
-        ["name" => "Beata Borysiuk", "role" => "Media"],
-        ["name" => "Damian Pawłowski", "role" => "Marketing"]
+        ["name" => "Zofia Dobrowolska", "role" => "Main coordinator", "email" => "zofia.dobrowolska@pwr.edu.pl"],
+        ["name" => "Beata Borysiuk", "role" => "Media", "email" => "beata.borysiuk@pwr.edu.pl"],
+        ["name" => "Damian Pawłowski", "role" => "Marketing", "email" => "damian.pawlowski@pwr.edu.pl"]
 ],
 
 
--- lang/pl.php
+++ lang/pl.php
@@ -13,14 +13,20 @@ $lang = array(
     "contact" => "Kontakt",
 
     "welcome_heading" => "Konferencja Doktorancka CYBER — Conference for Young Biomedical Engineering Researchers",
-    "welcome_text_1" => "Zapraszamy na Konferencję Doktorancką <strong>CYBER</strong> — wyjątkowe wydarzenie łączące młodych badaczy, doktorantów, studentów oraz specjalistów w dziedzinie inżynierii biomedycznej. Nasze zainteresowania obejmują takie obszary jak: neuronauka, optyka, obrazowanie medyczne, bioinformatyka, projektowanie leków, biomateriały oraz technologie przeciwdrobnoustrojowe. CYBER to przestrzeń do wymiany wiedzy i doświadczeń płynących z interdyscyplinarnej pracy naukowej. Chcemy zaprezentować najnowsze osiągnięcia naszej Katedry oraz zainspirować młodych badaczy do rozpoczęcia własnej drogi doktorskiej.",
-    "welcome_text_2" => "Podczas wykładów i dyskusji poszerzysz swoją wiedzę, poznasz najnowsze trendy w inżynierii biomedycznej i dowiesz się, jak możesz rozwijać swoją karierę naukową. Mamy nadzieję, że to właśnie tu znajdziesz motywację do podjęcia nowych wyzwań badawczych! Dołącz do nas! Wystarczy wypełnić formularz rejestracyjny. Widzimy się już <strong>23 czerwca 2025 roku w budynku D20 przy ulicy Zygmunta Janiszewskiego 8!</strong>",
+    "welcome_text_1" => "Zapraszamy na Konferencję Doktorancką <strong style='color: #1FA53E;'>CYBER</strong> — wyjątkowe wydarzenie łączące młodych badaczy, doktorantów, studentów oraz specjalistów w dziedzinie inżynierii biomedycznej. Nasze zainteresowania obejmują takie obszary jak: neuronauka, optyka, obrazowanie medyczne, bioinformatyka, projektowanie leków, biomateriały oraz technologie przeciwdrobnoustrojowe. CYBER to przestrzeń do wymiany wiedzy i doświadczeń płynących z interdyscyplinarnej pracy naukowej. Chcemy zaprezentować najnowsze osiągnięcia naszej Katedry oraz zainspirować młodych badaczy do rozpoczęcia własnej drogi doktorskiej.",
+    "welcome_text_2" => "Podczas wykładów i dyskusji poszerzysz swoją wiedzę, poznasz najnowsze trendy w inżynierii biomedycznej i dowiesz się, jak możesz rozwijać swoją karierę naukową. Mamy nadzieję, że to właśnie tu znajdziesz motywację do podjęcia nowych wyzwań badawczych! Dołącz do nas! Wystarczy wypełnić formularz rejestracyjny. Widzimy się już <strong style='color: #1FA53E;'>23 czerwca 2025 roku w budynku D20 przy ulicy Zygmunta Janiszewskiego 8!</strong>",
+    "welcome_important_title" => "<strong style='color: #1FA53E;'>WAŻNE</strong>",
+    "welcome_important_text1" => "Rejestracja dla uczestników aktywnych kończy się <strong style='color: #1FA53E;'>31 maja</strong>, a dla uczestników biernych <strong style='color: #1FA53E;'>18 czerwca</strong>.",
+    "welcome_important_text2" => "Informacje dotyczące akceptacji zgłoszeń zostaną przekazane do <strong style='color: #1FA53E;'>10 czerwca</strong>.",
+    "welcome_datePlace_title" => "Kiedy i gdzie?",
+    "welcome_datePlace_text" => "Konferencja odbędzie się 23 czerwca 2025 roku w budynku D20 przy ulicy Zygmunta Janiszewskiego 8!",
+
 
     "announcements" => "Aktualności",
-    "announcement_1_title" => "📅 Otwarcie rejestracji",
-    "announcement_1_text" => "Rejestracja na konferencję CYBER rozpocznie się <strong>15 maja</strong>. Zachęcamy do wcześniejszego zgłoszenia! Informacje na temat akceptacji zgłoszenia zostaną przekazane do <strong>10 czerwca</strong>. Bonus - konferencja jest <strong>bezpłatna</strong>!",
+    "announcement_1_title" => "📅 Informacje dotyczące rejestracji",
+    "announcement_1_text" => "Rejestracja na konferencję CYBER rozpocznie się <strong style='color: #1FA53E;'>15 maja</strong>. Zachęcamy do wcześniejszego zgłoszenia - czekamy na Wasze formularze do <strong style='color: #1FA53E;'>31 maja</strong>! Informacje na temat akceptacji zgłoszenia zostaną przekazane do <strong style='color: #1FA53E;'>10 czerwca</strong>. Bonus - konferencja jest <strong style='color: #1FA53E;'>bezpłatna</strong>!",
     "announcement_2_title" => "🎤 Wykład inaugarcyjny",
-    "announcement_2_text" => 'Z dumą ogłaszamy, że wykład inauguracyjny zatytułowany <i>"The positive face of protein and peptide aggregation: foldameric nanostructures and functional amyloids"</i> wygłosi Pani <strong>Dr Monika Szefczyk</strong> z Wydziału Chemicznego Politechniki Wrocławskiej',
+    "announcement_2_text" => 'Z dumą ogłaszamy, że wykład inauguracyjny zatytułowany <i style="color: #1FA53E;">"The positive face of protein and peptide aggregation: foldameric nanostructures and functional amyloids"</i> wygłosi Pani <strong style="color: #1FA53E;">Dr Monika Szefczyk</strong> z Wydziału Chemicznego Politechniki Wrocławskiej',
     "announcement_2_link" => 'Zapraszamy do zapoznania się z działalnością naszej prelegentki',
     "announcement_3_title" => "🧪 Znajdź nas w innych miejscach",
     "announcement_3_text" => "Obserwuj nasze wydarzenie na Facebooku - bądź na bieżąco!",
@@ -29,22 +35,17 @@ $lang = array(
     "time" => "Godzina",
     "event" => "Wydarzenie",
     "opening" => "Otwarcie konferencji",
-    "session1" => "Pierwsza sesja prezentacyjna",
+    "session1" => "Nauka widzenia i optometria",
     "coffee" => "Przerwa kawowa",
-    "session2" => "Druga sesja prezentacyjna",
-    "session3" => "Trzecia sesja prezentacyjna <br><span class='text-muted'>(7/8 prezentacji)</span>",
-    "lunch" => "<strong>Lunch</strong> <br><span class='text-muted'>(tylko dla prelegentów i profesorów)</span>",
-    "session4" => "Czwarta sesja prezentacyjna <br><span class='text-muted'>(7/8 prezentacji)</span>",
+    "session2" => "Neuronauka",
+    "session3" => "Biomateriały, obrazowanie biomedyczne i technologie diagnostyczne",
+    "lunch" => "<strong >Lunch</strong> <br><span class='text-muted'>(tylko dla prelegentów i profesorów)</span>",
+    "session4" => "Bioinformatyka i medycyna molekularna",
     "closing" => "Nagrody, podsumowanie, zakończenie konferencji",
 
     "registration_on_conf" => "Rejestracja na konferencję",
-    "registration_intro" => "Zarejestruj się już dziś na konferencję <strong>CYBER</strong>! Kliknij poniższy przycisk, aby przejść do formularza rejestracyjnego i zapoznać się ze szczegółami dotyczącymi udziału.",
+    "registration_intro" => "Zarejestruj się już dziś na konferencję <strong style='color: #1FA53E;'>CYBER</strong>! Kliknij poniższy przycisk, aby przejść do formularza rejestracyjnego i zapoznać się ze szczegółami dotyczącymi udziału.",
     "registration_button" => "Zarejestruj się tutaj",
-
-    "organizing_committee" => "Komitet organizacyjny",
-    "coordinator" => "Koordynator główny",
-    "logistics" => "Logistyka i kontakt z gośćmi",
-    "media" => "Media i promocja",
 
     "contact_intro" => "Masz pytania dotyczące konferencji? Skontaktuj się z nami za pomocą poniższego formularza lub napisz na adres e-mail.",
     "contact_name" => "Imię i nazwisko",
@@ -53,11 +54,14 @@ $lang = array(
     "contact_send" => "Wyślij wiadomość",
     "contact_alt" => "Ilustracja kontaktu",
 
+    "organizing_committee" => "Komitet organizacyjny",
+
     "committee_members" => [
-    ["name" => "Zofia Dobrowolska", "role" => "Główny koordynator"],
-    ["name" => "Beata Borysiuk", "role" => "Media"],
-    ["name" => "Damian Pawłowski", "role" => "Marketing"]
+    ["name" => "Zofia Dobrowolska", "role" => "Główny koordynator", "email" => "zofia.dobrowolska@pwr.edu.pl"],
+    ["name" => "Beata Borysiuk", "role" => "Media", "email" => "beata.borysiuk@pwr.edu.pl"],
+    ["name" => "Damian Pawłowski", "role" => "Marketing", "email" => "damian.pawlowski@pwr.edu.pl"]
 ],
+
 
 
 );
